1b187a09cd611d95d52dddcbd845847c74a85ebb
BewzB <blaz.bergant4@gmail.com>
2025-01-01 23:37:28 +0100

    Errors: Redid the errors package, so they have their own struct... Added common_errors.go, to store really common errors.

---
+++ pkg/errors/common_errors.go
@@ -0,0 +1,5 @@
+package errors
+
+var (
+	ErrInvalidFunctionArgument = NewErrorType("INVALID_FUNCTION_ARGUMENT", "An invalid function argument was provided")
+)
--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -2,12 +2,72 @@ package errors
 
 import (
 	"fmt"
+	"errors"
 )
 
-// Error wraps an error with a message. If the error is nil, it returns nil.
-func Error(err error, msg string) error {
-	if err == nil {
-		return nil
+// Error struct
+type appError struct {
+	Code string
+	Msg  string
+	Err  error
+}
+
+func (e *appError) Error() string {
+	if e.Err == nil {
+		return e.Msg
 	}
-	return fmt.Errorf("%s: %w", msg, err)
+	return fmt.Sprintf("%s: %s", e.Msg, e.Err.Error())
+}
+
+func (e *appError) Unwrap() error {
+	return e.Err
+}
+
+func (e *appError) Is(target error) bool {
+	t, ok := target.(*appError)
+	if !ok {
+		return false
+	}
+	return e.Code == t.Code
+}
+
+
+// FUNCTIONS FOR CREATING ERRORS
+
+// NewErrorType returns a new error type with the given code and a default message.
+func NewErrorType(code, default_msg string) *appError {
+	return &appError{
+		Code: code,
+		Msg:  default_msg,
+		Err:  nil,
+	}
+}
+
+// New returns a new checkable error.
+// External errors should be wrapped with this function.
+// If message is empty, the default message of the error type is used.
+func New(msg string, errorType *appError, err error) error {
+	if errorType == nil {
+		errorType = NewErrorType("UNKNOWN_ERROR", "An unknown error occurred")
+	}
+	if msg == "" {
+		msg = errorType.Msg
+	}
+	return &appError{
+		Code: errorType.Code,
+		Msg:  msg,
+		Err:  err,
+	}
+}
+
+// Wrap adds context to existing errors.
+func Wrap(msg string, err error) error {
+	return &appError{
+		Msg: msg,
+		Err: err,
+	}
+}
+
+func Is(err, target error) bool {
+	return errors.Is(err, target)
 }
