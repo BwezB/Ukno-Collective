6b50f7fef2e44a522ac6da6f0a56580f9174a425
BewzB <blaz.bergant4@gmail.com>
2025-01-03 22:05:48 +0100

    ERROR: Added new common_errors

--- pkg/errors/common_errors.go
+++ pkg/errors/common_errors.go
@@ -3,4 +3,6 @@ package errors
 var (
 	ErrInvalidFunctionArgument = NewErrorType("INVALID_FUNCTION_ARGUMENT", "An invalid function argument was provided")
 	ErrInternal = NewErrorType("INTERNAL_ERROR", "An internal error occurred")
+	ErrNotImplemented = NewErrorType("NOT_IMPLEMENTED", "This function is not implemented")
+	ErrCancelled = NewErrorType("CANCELLED", "The operation was cancelled")
 )
