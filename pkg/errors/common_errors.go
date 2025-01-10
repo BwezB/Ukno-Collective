54647abe3a2a9ca7ca8c74cb501ff95a1d08a914
BewzB <blaz.bergant4@gmail.com>
2025-01-10 22:00:59 +0100

    ERRORS: Added a bunch of common errors

--- pkg/errors/common_errors.go
+++ pkg/errors/common_errors.go
@@ -3,6 +3,9 @@ package errors
 var (
 	ErrInvalidFunctionArgument = NewErrorType("INVALID_FUNCTION_ARGUMENT", "An invalid function argument was provided")
 	ErrInternal = NewErrorType("INTERNAL_ERROR", "An internal error occurred")
+	ErrConnectionFailed = NewErrorType("CONNECTION_FAILED", "Failed to connect to the server")
 	ErrNotImplemented = NewErrorType("NOT_IMPLEMENTED", "This function is not implemented")
 	ErrCancelled = NewErrorType("CANCELLED", "The operation was cancelled")
+	ErrInvalidRequest = NewErrorType("INVALID_REQUEST", "Invalid request")
+	ErrHealthCheckFailed = NewErrorType("HEALTH_CHECK_FAILED", "Health check failed")
 )
